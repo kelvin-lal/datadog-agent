f4331685f158cc9f14dd6056d1d67bc31173beb4
Paul Cacheux <paul.cacheux@datadoghq.com>
2025-08-20 13:34:49 +0200

    fix image_util stub

--- comp/core/workloadmeta/collectors/sbomutil/image_util_stub.go
+++ comp/core/workloadmeta/collectors/sbomutil/image_util_stub.go
@@ -15,3 +15,33 @@ import (
 func UpdateSBOMRepoMetadata(sbom *workloadmeta.SBOM, _, _ []string) *workloadmeta.SBOM {
 	return sbom
 }
+
+// CompressSBOM converts a workloadmeta.SBOM into a workloadmeta.CompressedSBOM.
+func CompressSBOM(sbom *workloadmeta.SBOM) (*workloadmeta.CompressedSBOM, error) {
+	if sbom == nil {
+		return nil, nil
+	}
+
+	return &workloadmeta.CompressedSBOM{
+		Bom:                nil,
+		GenerationTime:     sbom.GenerationTime,
+		GenerationDuration: sbom.GenerationDuration,
+		Status:             sbom.Status,
+		Error:              sbom.Error,
+	}, nil
+}
+
+// UncompressSBOM converts a workloadmeta.CompressedSBOM into a workloadmeta.SBOM.
+func UncompressSBOM(csbom *workloadmeta.CompressedSBOM) (*workloadmeta.SBOM, error) {
+	if csbom == nil {
+		return nil, nil
+	}
+
+	return &workloadmeta.SBOM{
+		CycloneDXBOM:       nil,
+		GenerationTime:     csbom.GenerationTime,
+		GenerationDuration: csbom.GenerationDuration,
+		Status:             csbom.Status,
+		Error:              csbom.Error,
+	}, nil
+}
